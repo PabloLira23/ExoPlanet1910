eed5254e8683700de192deabf92cd68b2d4e6dd9
Andrey Santos <andrey05oficial@gmail.comcd>
2024-10-05 05:43:23 -0300

    Adicionada estilização da página index.

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
-<?php
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OrbitEX - Explore os Exoplanetas</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="shortcut icon" href="img/ico/Logo - OrbitEX.ico" type="image/x-icon">
 </head>
 <body>
-    <header style="width: 1536px;"> <!--barra preta--> 
-    <div class="img-nav"><img src="img/nav.png" alt="" style="width: 1530px;"></div> <!--Logo com linhas-->
+
+    <header> <!-- Barra de navegação completa -->
+        <div class="img-nav"><img src="img/nav.png" alt="" style="width: 1530px;"></div> <!-- Logo com linhas -->
 
         <nav>
             <ul class="nav-link">
@@ -20,16 +19,21 @@
             </ul>
         </nav>
 
-        <form action="" method="post">
-            <button class="botao" name=button>
-                <a href="login.php">Login</a>
-            </button>
-        </form>
-   </header>
-   
-   <div class="banner">
-    <video autoplay muted loop>
-        <source src="media/pag01/vídeo/planeta.mp4" type="video/mp4">
+        <button class="botao-nav">
+            <a href="login.php">Login</a>
+        </button>
+    </header>
+
+    <div class="conteudo-principal">
+        <p class="texto-1">Explore os Exoplanetas</p>
+        <p class="texto-2"><a href="login.php"><button class="botao-iniciar">INICIAR A JORNADA</button></a></p>
+        <p class="texto-3">"Descubra mundos além da nossa imaginação,<br>a um clique de distância".</p>
+    </div>
+
+    <video loop autoplay muted playsinline class="background-video">
+        <source src="./img/videook.mp4" type="video/mp4"> <!--TROCAR VÍDEO-->
+        Seu navegador não suporta a tag de vídeo.
     </video>
-   </div>
+
+</body>
 </html>
